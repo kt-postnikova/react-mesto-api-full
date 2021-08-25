78abf80c1c1986805087198ff2bf9a4cc6387b40
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-25 20:39:49 +0300

    Фикс ошибки CORS

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors({ origin: 'https://project.mesto.nomoredomains.rocks' }));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,7 +43,7 @@ app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  res.status(statusCode).send({ message: statusCode === 500 ? 'ffff' : message });
 
   next();
 });
