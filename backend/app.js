8e4a54bdc28bd5df1b90f513fecd86a7079b2b51
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-25 12:28:53 +0300

    Добавлен домен бэкенда в allowedCors

--- backend/app.js
+++ backend/app.js
@@ -70,7 +70,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
 
-    return res.end();
+    return res.status(200).send();
   }
 
   next();
