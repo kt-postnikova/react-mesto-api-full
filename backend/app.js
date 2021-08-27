dd9fb787d89fc9ff72f84477d90afc4d6313191e
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-27 11:26:41 +0300

    Фикс ошибки CORS

--- backend/app.js
+++ backend/app.js
@@ -56,30 +56,30 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-// const allowedCors = [
-//   'http://project.mesto.nomoredomains.rocks',
-//   'https://api.project.mesto.nomoredomains.club',
-//   'localhost:3000',
-// ];
+const allowedCors = [
+  'http://project.mesto.nomoredomains.rocks',
+  'https://api.project.mesto.nomoredomains.club',
+  'localhost:3000',
+];
 
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
 
-//     return res.status(200).send();
-//   }
+    return res.status(200).send();
+  }
 
-//   next();
-//   return null;
-// });
+  next();
+  return null;
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
