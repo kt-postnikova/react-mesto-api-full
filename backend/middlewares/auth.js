125a9c2240745336f3660cf5adf3d0cc552db7e5
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-27 13:22:48 +0300

    Фикс отображения инфо о пользователе

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,10 +4,9 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_SECRET = 'dev-key' } = process.env;
 
 module.exports = (req, res, next) => {
-  // const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-  // const token = authorization.replace('Bearer ', '');
-  const token = req.cookies.jwt;
+  const token = authorization.replace('Bearer ', '');
   let playload;
 
   try {
