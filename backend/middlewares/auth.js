facccfbaa55af24c359e9a49b152339b0620f2f6
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-28 18:39:15 +0300

    fix cors

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,9 +4,9 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { JWT_SECRET = 'dev-key' } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const { Authorization } = req.headers;
 
-  const token = authorization.replace('Bearer ', '');
+  const token = Authorization.replace('Bearer ', '');
   let playload;
 
   try {
