94ed65fd3a991fe397664afeb995cf9a2115d0f0
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-29 18:02:45 +0300

    fix cookie

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,9 +5,9 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  // const { authorization } = req.headers;
-  // const token = authorization.replace('Bearer ', '');
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  const token = authorization.replace('Bearer ', '');
+  // const token = req.cookies.jwt;
   let playload;
 
   try {
