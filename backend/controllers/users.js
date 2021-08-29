780efa8639041ae032bce2d3fe40a92553ba2503
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-29 18:08:47 +0300

    fix cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,7 +5,7 @@ const NotFoundError = require('../errors/NotFoundError');
 const ConflictError = require('../errors/ConflictError');
 const BadRequestError = require('../errors/BadRequestError');
 
-const { JWT_SECRET = 'dev-key' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUsers = (req, res, next) => {
   User.find({})
@@ -58,9 +58,9 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key', { expiresIn: '7d' });
 
-      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true, sameSite: true, secure: true }).send({ token });
+      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true }).send({ message: 'token' });
     })
     .catch(next);
 };
