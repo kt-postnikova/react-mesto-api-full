e5a31ac1619e68239d6d5aed89a28012eb47b6f0
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-30 17:37:39 +0300

    Фикс выхода

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,7 @@ app.use(cors({
   methods: ['POST', 'DELETE', 'GET', 'PUT', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  optionsSuccessStatus: 204,
 }));
 
 app.use(bodyParser.json());
