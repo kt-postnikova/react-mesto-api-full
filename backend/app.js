89edbcdffdf21467a33f691acfa597fef6efa5b4
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-30 15:47:17 +0300

    Фикс выхода

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { loginValidator, registrationValidator } = require('./middlewares/validation');
 const NotFoundError = require('./errors/NotFoundError');
-const signOut = require('./controllers/users');
+// const signOut = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
@@ -32,7 +32,7 @@ app.use(cookieParser());
 
 app.post('/signin', loginValidator, login);
 app.post('/signup', registrationValidator, createUser);
-app.delete('/signout', signOut);
+// app.delete('/signout', signOut);
 
 app.use(auth);
 app.use('/', userRouter);
