da40108c57702d7c4e10b6d28a3bfd161309b84b
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-30 16:27:58 +0300

    Фикс выхода

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -101,7 +101,7 @@ const getUserInfo = (req, res, next) => {
 };
 
 const signOut = (req, res, next) => {
-  res.clearCookie('jwt').send('fsfsdf');
+  res.clearCookie('jwt').send({ message: 'dsfsfs' });
   next();
 };
 
