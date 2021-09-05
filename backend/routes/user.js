d91061e80ac5a76fd4adb73a29cf3fa37e55d756
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-05 15:54:25 +0300

    Фикс валидации аватара

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,13 +7,13 @@ const {
   updateUserProfile,
   updateUserAvatar,
 } = require('../controllers/users');
-const { idValidator, userInfoValidator } = require('../middlewares/validation');
+const { idValidator, userInfoValidator, avatarValidation } = require('../middlewares/validation');
 
 usersRouter.get('/users', getUsers);
 usersRouter.get('/users/me', getUserInfo);
 usersRouter.get('/users/:id', idValidator, getUserById);
 // usersRouter.delete('/users/:id', deleteUser);
 usersRouter.patch('/users/me', userInfoValidator, updateUserProfile);
-usersRouter.patch('/users/me/avatar', userInfoValidator, updateUserAvatar);
+usersRouter.patch('/users/me/avatar', avatarValidation, updateUserAvatar);
 
 module.exports = usersRouter;
